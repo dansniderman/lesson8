8a42c7851af43929cb51f6c095517ad63f2259ca
Dan Sniderman <dan.sniderman@3cloudsolutions.com>
2025-05-30 14:01:12 -0500

    feat: add divide function and corresponding tests; update version to 1.1.0

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -11,3 +11,11 @@ test("adding two values", () => {
 test("subtracting two values", () => {
     expect(appOperations.subtract(10, 4)).toBe(6);
 });
+
+test("dividing two values", () => {
+    expect(appOperations.divide(20, 4)).toBe(5);
+});
+test("dividing by zero throws error", () => {
+    expect(() => appOperations.divide(10, 0)).toThrow("Cannot divide by zero");
+});
+
